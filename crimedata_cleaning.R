0157fdee9ac9cd09e3d138c0b5c79a55a2e79d14
Rebecca Peer <Becca@Rebeccas-MacBook-Pro.local>
2017-11-01 14:27:52 -0700

    Consolidate crime code descriptions

--- crimedata_cleaning.R
+++ crimedata_cleaning.R
@@ -74,7 +74,6 @@ df$Crime.Code.4 = as.factor(df$Crime.Code.4)
 df$Weekday = weekdays(df$Date.Occurred)
 df$Month = months(df$Date.Occurred)
 
-
 #create testing dataset 
 dftest = df[1:100,2:29]
 dftest = dftest[,-9]
@@ -95,4 +94,49 @@ dftest = dftest[,-11]
 
 write.csv(dftest, "test.csv", row.names = FALSE)
 
+#consolidate crimes - lots of cleanup here!
+dfcrimes = df
+dfcrimes$Consolidated.Description[dfcrimes$Crime.Code.Description == "CRUELTY TO ANIMALS"] = "ANIMAL CREULTY"
+dfcrimes$Consolidated.Description[dfcrimes$Crime.Code.Description == "ARSON"] = "ARSON"
+dfcrimes$Consolidated.Description[dfcrimes$Crime.Code %in% c("236","622","623","624","625","626","627")] = "ASSAULT"
+dfcrimes$Consolidated.Description[dfcrimes$Crime.Code %in% c("230","231")] = "ASSAULT WITH A DEADLY WEAPON"
+dfcrimes$Consolidated.Description[dfcrimes$Crime.Code.Description == "BATTERY WITH SEXUAL CONTACT"] = "BATTERY WITH SEXUAL CONTACT"
+dfcrimes$Consolidated.Description[dfcrimes$Crime.Code.Description == "BOMB SCARE"] = "BOMB SCARE" 
+dfcrimes$Consolidated.Description[dfcrimes$Crime.Code.Description == "BRIBERY"] = "BRIBERY"
+dfcrimes$Consolidated.Description[dfcrimes$Crime.Code %in% c("310","320","330","410")] = "BURGLARY"
+dfcrimes$Consolidated.Description[dfcrimes$Crime.Code.Description == "CHILD ABUSE (PHYSICAL) - AGGRAVATED ASSAULT"] = "CHILD ABUSE"
+dfcrimes$Consolidated.Description[dfcrimes$Crime.Code.Description == "CHILD ANNOYING (17YRS & UNDER)"] = "CHILD ANNOYANCE"
+dfcrimes$Consolidated.Description[dfcrimes$Crime.Code %in% c("237","870")] = "CHILD NEGLECT"
+dfcrimes$Consolidated.Description[dfcrimes$Crime.Code.Description == "CONSPIRACY"] = "CONSPIRACY"
+dfcrimes$Consolidated.Description[dfcrimes$Crime.Code.Description == "CONTEMPT OF COURT"] = "CONTEMPT OF COURT"
+dfcrimes$Consolidated.Description[dfcrimes$Crime.Code %in% c("651","652","660")] = "COUNTERFEIT"
+dfcrimes$Consolidated.Description[dfcrimes$Crime.Code.Description == "CRM AGNST CHLD (13 OR UNDER) (14-15 & SUSP 10 YRS OLDER)0060"] = "CRIME AGAINST CHILD"
+dfcrimes$Consolidated.Description[dfcrimes$Crime.Code %in% c("880","886")] = "DISTURBING THE PEACE"
+dfcrimes$Consolidated.Description[dfcrimes$Crime.Code.Description == "DRUGS, TO A MINOR"] = "DRUGS, TO A MINOR"
+dfcrimes$Consolidated.Description[dfcrimes$Crime.Code.Description == "EXTORTION"] = "EXTORTION" 
+dfcrimes$Consolidated.Description[dfcrimes$Crime.Code.Description == "DOCUMENT FORGERY / STOLEN FELONY"] = "FORGERY"
+dfcrimes$Consolidated.Description[dfcrimes$Crime.Code %in% c("653","654")] = "FRAUD"
+dfcrimes$Consolidated.Description[dfcrimes$Crime.Code.Description == "CRIMINAL HOMICIDE"] = "HOMICIDE"
+dfcrimes$Consolidated.Description[dfcrimes$Crime.Code.Description == "THEFT OF IDENTITY"] = "IDENTITY THEFT" 
+dfcrimes$Consolidated.Description[dfcrimes$Crime.Code %in% c("910","920","922")] = "KIDNAPPING"
+dfcrimes$Consolidated.Description[dfcrimes$Crime.Code %in% c("762","956")] = "LEWD CONDUCT"
+dfcrimes$Consolidated.Description[dfcrimes$Crime.Code %in% c("435","436")] = "LYNCHING"
+dfcrimes$Consolidated.Description[dfcrimes$Crime.Code %in% c("113")] = "MANSLAUGHTER"
+dfcrimes$Consolidated.Description[dfcrimes$Crime.Code %in% c("432","433","647","661","890","924","926","946","954")] = "MISCELLANEOUS CRIME"
+dfcrimes$Consolidated.Description[dfcrimes$Crime.Code %in% c("932")] = "PEEPING TOM"
+dfcrimes$Consolidated.Description[dfcrimes$Crime.Code %in% c("805","806")] = "PIMPING"
+dfcrimes$Consolidated.Description[dfcrimes$Crime.Code %in% c("933")] = "PROWLER"
+dfcrimes$Consolidated.Description[dfcrimes$Crime.Code %in% c("121","122","815","821")] = "RAPE"
+dfcrimes$Consolidated.Description[dfcrimes$Crime.Code %in% c("438")] = "RECKLESS DRIVING"
+dfcrimes$Consolidated.Description[dfcrimes$Crime.Code %in% c("931")] = "REPLICA FIREARMS"
+dfcrimes$Consolidated.Description[dfcrimes$Crime.Code %in% c("437")] = "RESISTING ARREST"
+dfcrimes$Consolidated.Description[dfcrimes$Crime.Code %in% c("210","220","331","341","343","345","347","349","350","351","352","353","420","421","440","441","442","443","444","445","446","450","451","452","470","471","473","474","475","480","485","487","510","520","662","664","666","668","670","950","951")] = "ROBBERY"
+dfcrimes$Consolidated.Description[dfcrimes$Crime.Code %in% c("850","820","830","840","948","810")] = "SEXUAL CRIME"
+dfcrimes$Consolidated.Description[dfcrimes$Crime.Code %in% c("250","251","753")] = "SHOTS FIRED"
+dfcrimes$Consolidated.Description[dfcrimes$Crime.Code %in% c("763")] = "STALKING"
+dfcrimes$Consolidated.Description[dfcrimes$Crime.Code %in% c("928","930")] = "THREAT"
+dfcrimes$Consolidated.Description[dfcrimes$Crime.Code %in% c("888")] = "TRESPASSING"
+dfcrimes$Consolidated.Description[dfcrimes$Crime.Code %in% c("740","745")] = "VANDLISM"
+dfcrimes$Consolidated.Description[dfcrimes$Crime.Code %in% c("900","901","902","906")] = "VIOLATION OF COURT ORDER"
+dfcrimes$Consolidated.Description[dfcrimes$Crime.Code %in% c("761","756")] = "WEAPONS POSESSION"
 
